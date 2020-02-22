7dcb89a7a3ea6a684da129ba254ef3bbf487d6d8
abedi1 <56488576+abedi1@users.noreply.github.com>
2020-02-22 16:15:11 -0500

    Finished the poseNet Tensorflow component

--- Extension/js/pose.js
+++ Extension/js/pose.js
@@ -1,14 +1,108 @@
+function distance(p1, p2) {
+  return Math.sqrt(Math.pow((p1.x-p2.x),2) + Math.pow((p1.y-p2.y),2));
+}
+
+function poseDifferences(good, bad) {
+  differences = {noseL: good.noseLeft/bad.noseLeft, noseR: good.noseRight/bad.noseRight,
+  rL: good.leftRight/bad.leftRight};
+
+  return differences;
+}
+
+function distancesFromPose(pose){
+  return {noseLeft: distance(pose.keypoints[0].position,pose.keypoints[5].position),
+    noseRight: distance(pose.keypoints[0].position,pose.keypoints[6].position),
+    leftRight: distance(pose.keypoints[5].position,pose.keypoints[6].position)};
+}
+
+function isSlouching(diff){
+
+  // if nose difference decreases by 10% or should distances increases by 10%
+  if(diff.noseL > 1.10 || diff.noseR> 1.10 || diff.rL <0.9){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+function calibrateGoodPosture (elementId) {
+  var flipHorizontal = false;
+  var goodValues;
+  var goodPosture = document.getElementById(elementId); // this line refers to how i get the image
+
+  posenet.load().then(function(net) {
+    const poseG = net.estimateSinglePose(goodPosture, {
+      flipHorizontal: true
+    });
+    return poseG;
+  }).then(function(poseG){
+    console.log("GoodPosture");
+    console.log(poseG);
+
+    goodValues = distancesFromPose(poseG);
+    return goodValues;
+  });
+}
+
+function testPosture (goodPostureValues, elementId){
+  var flipHorizontal = false;
+  var badValues;
+  var currentPosture = document.getElementById(elementId);
+  posenet.load().then(function(net) {
+    const poseB = net.estimateSinglePose(currentPosture, {
+      flipHorizontal: true
+    });
+    return poseB;
+  }).then(function(poseB){
+    console.log("currentPosture");
+    console.log(poseB);
+
+    currentValues= distancesFromPose(poseB)
+    console.log(currentValues);
+
+    console.log("Percent Differences");
+    console.log(poseDifferences(goodValues,currentValues));
+
+    return(isSlouching(poseDifferences(goodPostureValues,badValues)));
+  })
+}
+
     console.log("Hello");
     var flipHorizontal = false;
+    var goodValues;
+    var badValues;
+    var goodPosture = document.getElementById('postureGood');
+    var badPosture = document.getElementById('postureBad');
 
-    var imageElement = document.getElementById('cat');
-
+    // doing posture things on the goodPosture image
     posenet.load().then(function(net) {
-      const pose = net.estimateSinglePose(imageElement, {
+      const poseG = net.estimateSinglePose(goodPosture, {
         flipHorizontal: true
       });
-      return pose;
-    }).then(function(pose){
-      console.log(pose);
-      console.log("Nose Coordinates: "+ pose[0]);
+      return poseG;
+    }).then(function(poseG){
+      console.log("GoodPosture");
+      console.log(poseG);
+
+      goodValues = distancesFromPose(poseG);
+      console.log(goodValues);
+    });
+
+      //bad posture
+    posenet.load().then(function(net) {
+      const poseB = net.estimateSinglePose(badPosture, {
+        flipHorizontal: true
+      });
+      return poseB;
+    }).then(function(poseB){
+      console.log("badPosture");
+      console.log(poseB);
+
+      badValues= distancesFromPose(poseB)
+      console.log(badValues);
+
+      console.log("Percent Differences");
+      console.log(poseDifferences(goodValues,badValues));
+
+      console.log(isSlouching(poseDifferences(goodValues,badValues)))
     })
