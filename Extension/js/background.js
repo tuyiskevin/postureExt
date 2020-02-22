b8ab457bf04cb2fbf34a3ad1e27c2499c27c1b01
WillyD <sopwill3@gmail.com>
2020-02-22 13:17:47 -0500

    new popup

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -3,14 +3,11 @@
 // Runs at on Installation (initial)
 chrome.runtime.onInstalled.addListener(function() {
 
-
   // There are a bunch of functions like this one that do various things,
   // We'll find them as we need them.
     // Saves some data to "chrome.storage" which is accessable by all files in extension
     // chrome.storage.sync.set({data: 'value'}, function() {
     //
     // });
-
-
 
   });
